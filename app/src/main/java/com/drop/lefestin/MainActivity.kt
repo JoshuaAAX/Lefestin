22b72c79e0ad3b9b0a9e6a0aeee7015679f542b7
JoshuaAAX <joshua.chicame@correounivalle.edu.co>
2023-10-10 23:16:13 -0500

    add bottomAppBar

--- app/src/main/java/com/drop/lefestin/MainActivity.kt
+++ app/src/main/java/com/drop/lefestin/MainActivity.kt
@@ -4,7 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,11 +39,66 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BottomAppBarWithIcons()
+
                 }
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomAppBarWithIcons() {
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(
+            ) {
+                IconButton(
+                    onClick = { /* Handle icon 1 click */ },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add Icon",
+                    )
+                }
+                IconButton(
+                    onClick = { /* Handle icon 2 click */ },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Check,
+                        contentDescription = "Check Icon"
+                    )
+                }
+                IconButton(
+                    onClick = { /* Handle icon 3 click */ },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Edit,
+                        contentDescription = "Edit Icon"
+                    )
+                }
+                IconButton(
+                    onClick = { /* Handle icon 4 click */ },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Custom Icon"
+                    )
+                }
+            }
+        },
+        content = { innerPadding ->
+            Text(
+                modifier = Modifier.padding(innerPadding),
+                text = "Example of a scaffold with a bottom app bar and four icons."
+            )
+        }
+    )
 }
 
 @Composable
