0f2dd16618b5c07d691147883a719c967ab71295
JoshuaAAX <joshua.chicame@correounivalle.edu.co>
2023-12-01 16:41:27 -0500

    fix fix

--- app/src/main/java/com/drop/lefestin/ViewModels/SupabaseAuthViewModel.kt
+++ app/src/main/java/com/drop/lefestin/ViewModels/SupabaseAuthViewModel.kt
@@ -1,21 +1,19 @@
 package com.drop.lefestin.ViewModels
 
 import android.content.Context
-import android.content.SharedPreferences
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
-import co.touchlab.kermit.Severity
-import com.drop.lefestin.SupabaseHelper
-import com.drop.lefestin.SupabaseHelper.client
+import com.drop.lefestin.data.model.UserState
+import com.drop.lefestin.data.network.SupabaseClient.client
+import com.drop.lefestin.utils.SharedPreferenceHelper
+import io.github.jan.supabase.exceptions.RestException
 import io.github.jan.supabase.gotrue.gotrue
 import io.github.jan.supabase.gotrue.providers.builtin.Email
-import io.ktor.client.engine.KTOR_DEFAULT_USER_AGENT
 import kotlinx.coroutines.launch
 
-class SupabaseAuthViewModel: ViewModel() {
+class SupabaseAuthViewModel : ViewModel() {
     private val _userState = mutableStateOf<UserState>(UserState.Loading)
     val userState: State<UserState> = _userState
 
@@ -23,83 +21,91 @@ class SupabaseAuthViewModel: ViewModel() {
         context: Context,
         userEmail: String,
         userPassword: String,
-        ){
+    ) {
         viewModelScope.launch {
             try {
-                SupabaseHelper.client.gotrue.signUpWith(Email){
+                _userState.value = UserState.Loading
+                client.gotrue.signUpWith(Email) {
                     email = userEmail
                     password = userPassword
                 }
-                saveToke(context)
-                _userState.value= UserState.Success("registered user success")
-
-            }catch (e: Exception){
-                _userState.value = UserState.Error("Error: ${e.message}")
+                saveToken(context)
+                _userState.value = UserState.Success("Registered successfully!")
+            } catch(e: Exception) {
+                _userState.value = UserState.Error(e.message ?: "")
             }
+
         }
     }
-
-    private fun saveToke(context: Context) {
+    private fun saveToken(context: Context) {
         viewModelScope.launch {
-            val accessToken = client.gotrue.currentAccessTokenOrNull() ?: ""
-            val sharedProf = SharedPreferenceHelper(context)
-            sharedProf.saveStringData("accessToken", accessToken)
+            val accessToken = client.gotrue.currentAccessTokenOrNull()
+            val sharedPref = SharedPreferenceHelper(context)
+            sharedPref.saveStringData("accessToken",accessToken)
         }
 
     }
 
-    private fun getToken(context: Context) :String? {
-        val sharedProf = SharedPreferenceHelper(context)
-        return sharedProf.getStringData("accessToken")
+    private fun getToken(context: Context): String? {
+        val sharedPref = SharedPreferenceHelper(context)
+        return sharedPref.getStringData("accessToken")
     }
 
     fun login(
         context: Context,
         userEmail: String,
-        userPassword: String
-    ){
+        userPassword: String,
+    ) {
         viewModelScope.launch {
             try {
+                _userState.value = UserState.Loading
                 client.gotrue.loginWith(Email) {
                     email = userEmail
                     password = userPassword
                 }
-                saveToke(context)
-                _userState.value = UserState.Success("Logged in sccesfully")
+                saveToken(context)
+                _userState.value = UserState.Success("Logged in successfully!")
             } catch (e: Exception) {
-                _userState.value = UserState.Error("Error: ${e.message}")
+                _userState.value = UserState.Error(e.message ?: "")
+            }
+
+        }
+    }
+
+    fun logout(context: Context) {
+        val sharedPref = SharedPreferenceHelper(context)
+        viewModelScope.launch {
+            try {
+                _userState.value = UserState.Loading
+                client.gotrue.logout()
+                sharedPref.clearPreferences()
+                _userState.value = UserState.Success("Logged out successfully!")
+            } catch (e: Exception) {
+                _userState.value = UserState.Error(e.message ?: "")
             }
         }
     }
 
-    fun logout() {
-        viewModelScope.launch {
-            try {
-                client.gotrue.logout()
-                _userState.value = UserState.Success("logged out succesfully")
-            } catch (e: Exception){
-                _userState.value = UserState.Error("Error: ${e.message}")
-            }
-        }
-    }
 
     fun isUserLoggedIn(
-        context: Context
-    ){
+        context: Context,
+    ) {
         viewModelScope.launch {
             try {
+                _userState.value = UserState.Loading
                 val token = getToken(context)
-                if(token.isNullOrEmpty()){
-                    _userState.value = UserState.Error("User is not Logges in")
+                if(token.isNullOrEmpty()) {
+                    _userState.value = UserState.Success("User not logged in!")
                 } else {
                     client.gotrue.retrieveUser(token)
                     client.gotrue.refreshCurrentSession()
-                    saveToke(context)
-                    _userState.value = UserState.Success("user is alreasy logge in")
+                    saveToken(context)
+                    _userState.value = UserState.Success("User already logged in!")
                 }
-            } catch (e : Exception) {
-                _userState.value = UserState.Error("Error: ${e.message}")
+            } catch (e: RestException) {
+                _userState.value = UserState.Error(e.error)
             }
         }
     }
+
 }
